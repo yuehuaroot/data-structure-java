01019b8b244f2ed59fb6be9f1e9fa78d8b0f2727
liuyuehua <519927235@qq.com>
2020-04-05 22:01:55 +0800

    单链表按顺序插入节点

--- src/main/java/com/yuehua/datastructure/linkedlist/SingleLinkedListDemo.java
+++ src/main/java/com/yuehua/datastructure/linkedlist/SingleLinkedListDemo.java
@@ -11,16 +11,29 @@ public class SingleLinkedListDemo {
     public static void main(String[] args) {
         // 测试
         // 创建节点  没有考虑排名
-        HeroNode heroNode = new HeroNode(1, "宋江", "及时雨", null);
-        HeroNode heroNode1 = new HeroNode(2, "卢俊义", "玉麒麟", null);
-        HeroNode heroNode2 = new HeroNode(3, "吴用", "智多星", null);
-        HeroNode heroNode3 = new HeroNode(4, "林冲", "豹子头", null);
+//        HeroNode heroNode = new HeroNode(1, "宋江", "及时雨", null);
+//        HeroNode heroNode1 = new HeroNode(2, "卢俊义", "玉麒麟", null);
+//        HeroNode heroNode2 = new HeroNode(3, "吴用", "智多星", null);
+//        HeroNode heroNode3 = new HeroNode(4, "林冲", "豹子头", null);
+//        SingleLinkedList singleLinkedList = new SingleLinkedList();
+//        singleLinkedList.addNode(heroNode);
+//        singleLinkedList.addNode(heroNode2);
+//        singleLinkedList.addNode(heroNode1);
+//        singleLinkedList.addNode(heroNode3);
+//        singleLinkedList.listNode();
+
+        HeroNode heroNode1 = new HeroNode(1, "宋江", "及时雨", null);
+        HeroNode heroNode3 = new HeroNode(3, "吴用", "智多星", null);
+        HeroNode heroNode4 = new HeroNode(4, "林冲", "豹子头", null);
+        HeroNode heroNode2 = new HeroNode(2, "卢俊义", "玉麒麟", null);
         SingleLinkedList singleLinkedList = new SingleLinkedList();
-        singleLinkedList.addNode(heroNode);
-        singleLinkedList.addNode(heroNode2);
-        singleLinkedList.addNode(heroNode1);
-        singleLinkedList.addNode(heroNode3);
+        singleLinkedList.addByOrder(heroNode1);
+        singleLinkedList.addByOrder(heroNode2);
+        singleLinkedList.addByOrder(heroNode3);
+        singleLinkedList.addByOrder(heroNode4);
+//        singleLinkedList.addByOrder(heroNode3);
         singleLinkedList.listNode();
+
 
     }
 }
@@ -31,9 +44,9 @@ public class SingleLinkedListDemo {
 @NoArgsConstructor
 class HeroNode {
     // 编号
-    private int no;
-    private String name;
-    private String nickName;
+    public int no;
+    public String name;
+    public String nickName;
 
     @Override
     public String toString() {
@@ -85,5 +98,41 @@ class SingleLinkedList {
             temp = temp.next;
         }
     }
+
+    /**
+     * 按照顺序加入链表 需要循环获取
+     * 1.加入到哪里
+     * 2.怎么加入
+     */
+    public void addByOrder(HeroNode hero) {
+        HeroNode temp = head;
+        // 退出循环的标志
+        boolean flag = false;
+        while (true) {
+            // 循环到最后一个节点，退出
+            if (temp.next == null) {
+                break;
+            }
+            // 已经加入成功活着 存在了 也要退出
+            if (temp.no == hero.no) {
+                break;
+            }
+            // 找到了
+            if (temp.next.no > hero.no) {
+                flag = true;
+                break;
+            }
+            temp = temp.next;
+
+        }
+        if (flag) {
+            System.out.println("英雄=>" + hero.toString() + "已经存在，不能加入");
+        }{
+
+            hero.next = temp.next;
+            temp.next = hero;
+        }
+    }
+
 
 }
